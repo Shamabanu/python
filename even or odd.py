792d8df7155ad4945ee8ec5dbab3b239a1771e66
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-09-03 23:25:01 -0700

    Rename even or odd or invalid.py to even or odd.py

---
+++ even or odd.py
@@ -0,0 +1,8 @@
+n=input()
+if(n.isalpha()):
+    if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u' or n=='A' or n=='E' or n=='I' or n=='O' or n=='U'):
+        print("Vowel")
+    else:
+        print("Consnant")
+else:
+    print("Invalid")

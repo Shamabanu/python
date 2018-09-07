c92a78bb3fa4068f6d89c6b255e3303628e795d8
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-09-07 08:13:10 -0700

    Create armstrong numbers between two intervals.py

---
+++ armstrong numbers between two intervals.py
@@ -0,0 +1,14 @@
+lower = int(input())
+upper = int(input())
+
+for num in range(lower, upper + 1):
+   order = len(str(num))
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)

e601c1435854c10a84855967b3c15f549a13b116
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-11-22 12:58:05 +0530

    Create num to words.py

---
+++ num to words.py
@@ -0,0 +1,21 @@
+n=int(input())
+if(n==1):
+    print("One")
+if(n==2):
+    print("Two")
+if(n==3):
+    print("Three")
+if(n==4):
+    print("Four")
+if(n==5):
+    print("Five")
+if(n==6):
+    print("Six")
+if(n==7):
+    print("Seven")
+if(n==8):
+    print("Eight")
+if(n==9):
+    print("Nine")
+if(n==10):
+    print("Ten")

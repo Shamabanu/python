213deb33cd672a20c78e34e16ed465f750fa498f
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-11-27 21:38:03 +0530

    Update reverse a list.py

--- reverse a list.py
+++ reverse a list.py
@@ -1,6 +1,6 @@
-m=int(input())
-n=[]
-for i in range(0,m):
-    n.append(int(input()))
-list.reverse(n)
-print(n)
+M=int(input())
+N=[]
+for i in range(0,M):
+    N.append(int(input()))
+list.reverse(N)
+print(N)

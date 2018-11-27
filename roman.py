7e8459fbb91c2eccb6bfaa280a09dca5df3e61b1
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-11-27 21:53:53 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,12 +1,12 @@
 val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-s = str(input())
-s = s.upper()
-t = 0
-while s:
-    if len(s) == 1 or val[s[0]] >= val[s[1]]:
-        t += val[s[0]]
-        s = s[1:]
+S = str(input())
+S = S.upper()
+a = 0
+while S:
+    if len(S) == 1 or val[S[0]] >= val[S[1]]:
+        a += val[S[0]]
+        S = S[1:]
     else:
-        t += val[s[1]] - val[s[0]]
-        s = s[2:]
-print (t)
+        a += val[S[1]] - val[S[0]]
+        S = S[2:]
+print (a)

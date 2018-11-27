d1eac87c2886b499d7385b40ed9eddf6bd37f55a
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-11-27 20:48:37 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -3,6 +3,6 @@ if(N.isalpha()):
     if(N=='a' or N=='e' or N=='i' or N=='o' or N=='u' or N=='A' or N=='E' or N=='I' or N=='O' or N=='U'):
         print("Vowel")
     else:
-        print("Consnant")
+        print("Consonant")
 else:
     print("Invalid")

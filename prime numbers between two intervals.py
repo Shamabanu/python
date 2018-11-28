c7f2ab53168dc3e35271398279d644a1f83d6597
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-11-28 11:28:06 +0530

    Update prime numbers between two intervals.py

--- prime numbers between two intervals.py
+++ prime numbers between two intervals.py
@@ -1,9 +1,10 @@
-lower = int(raw_input())
-upper = int(raw_input())
-for num in range(lower,upper):
-   if num > 1:
+Lower,Upper=input().split()
+Lower=int(Lower)
+Upper=int(Upper)
+for num in range(Lower+1,Upper):
+   if(num > 1):
        for i in range(2,num):
            if (num % i) == 0:
                break
        else:
-           print(num)
+           print(num,end=" ")

da1b47f2331f8dbd2337214c4fb6c777cde39b12
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-11-28 12:45:32 +0530

    Update min_to_hours.py

--- min_to_hours.py
+++ min_to_hours.py
@@ -1,6 +1,6 @@
-len=int(input())
-if len > 0:
-    len = "%d:%02d" % (len / 60, len % 60)
+Len=int(input())
+if(Len > 0):
+    Len = "%d %02d" % (Len / 60, Len % 60)
 else:
-    len = ""
-print(len)
+    Len = ""
+print(Len)

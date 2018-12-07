939d9a42dcb46dfe1015ff34a5f14c183733def3
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-12-07 12:55:09 +0530

    Create hut.py

---
+++ hut.py
@@ -0,0 +1,13 @@
+i=int(input())
+l=input().split()
+a=[]
+s=len(l)
+for su in range(0,s):
+    p=int(l[su])
+    if(p==su):
+        a.append(su)
+if(len(a)!=0):
+    s=' '.join(map(str,a))
+    print(s)
+else:
+    print("-1")

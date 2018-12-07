24721010666856d779ca82d17be52bbbc2f34483
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-12-07 12:08:39 +0530

    Update differ.py

--- differ.py
+++ differ.py
@@ -1,13 +1,13 @@
 h,j=map(str,input().split())
-count=0
+co=0
 for x in range(0,len(h)):
     c=h[x]
     d=j[x]
 if(c==d):
-        count=count
+        co=co
 else:
-    count=count+1
-if(count==1):
+    co=co+1
+if(co==1):
             print("yes")
 else:
     print("no")

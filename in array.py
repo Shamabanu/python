45f1fe7f8679f5d9bd5c9737ebd40c68251b3d87
Shamabanu <42835951+Shamabanu@users.noreply.github.com>
2018-12-10 13:03:35 +0530

    Update in array.py

--- in array.py
+++ in array.py
@@ -1,20 +1,20 @@
 n=int(input())
-arr=input().split()
-arr.sort() 
-max = 1; res = arr[0]; cur = 1
+a=input().split()
+a.sort() 
+max = 1; res = a[0]; cur = 1
 for i in range(1, n):  
-    if (arr[i] == arr[i - 1]): 
+    if (a[i] == a[i - 1]): 
         cur += 1
               
     else : 
         if (cur > max):  
             max = cur 
-            res = arr[i - 1] 
+            res = a[i - 1] 
               
         curr_count = 1
 
 if (cur > max): 
       
     max = cur 
-    res = arr[n - 1] 
+    res = a[n - 1] 
 print(res) 

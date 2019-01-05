9e390001a51a3461a24ee77fba594ed6c1d6afeb
SHAMA SHAIK <42835951+Shamabanu@users.noreply.github.com>
2019-01-05 21:19:32 +0530

    Create formation of square.py

---
+++ formation of square.py
@@ -0,0 +1,8 @@
+p1,q1=map(int,input().split())
+p2,q2=map(int,input().split())
+p3,q3=map(int,input().split())
+p4,q4=map(int,input().split())
+if(p1+p2+p3+p4==q1+q2+q3+q4):
+    print("yes")
+else:
+    print("no")

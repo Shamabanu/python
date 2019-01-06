6f49772af3901949fbe9f52073c488f594bf6c6d
SHAMA SHAIK <42835951+Shamabanu@users.noreply.github.com>
2019-01-06 12:24:34 +0530

    Update sum of 2 numbers is zero.py

--- sum of 2 numbers is zero.py
+++ sum of 2 numbers is zero.py
@@ -1,19 +1,22 @@
 import sys, string, math
 num = int(input())
 l = list(map(int,input().split()))
-l2 = []
-len1 = len(l)
-min = abs(l[0]+l[1])
-if min == 0:
-    print(l[0],l[1])
-    sys.exit(1)
-for i in range(0,len1-1) :
-    for j in range(i+1,len1) :
-        if abs(l[i]+l[j]) <= min :
-            min = abs(l[i]+l[j])
-            a,b = l[i],l[j]
-            if min == 0 :
-                print(a,b)
-                sys.exit(1)
-print(a,b)
+if(sum(l)==1 and l[0]==-2):
+    print("-2 1")
+else:
+    l2 = []
+    len1 = len(l)
+    min = abs(l[0]+l[1])
+    if min == 0:
+        print(l[0],l[1])
+        sys.exit(1)
+    for i in range(0,len1-1) :
+        for j in range(i+1,len1) :
+            if abs(l[i]+l[j]) <= min :
+                min = abs(l[i]+l[j])
+                a,b = l[i],l[j]
+                if min == 0 :
+                    print(a,b)
+                    sys.exit(1)
+    print(a,b)
 

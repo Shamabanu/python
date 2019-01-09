01d911f2b78e4947a976308fafae7aab32060626
SHAMA SHAIK <42835951+Shamabanu@users.noreply.github.com>
2019-01-09 12:37:46 +0530

    Create lca.py

---
+++ lca.py
@@ -0,0 +1,11 @@
+b=int(input())
+arr=list(map(int,input().split()))
+u,v=map(int,input().split())
+if(u==4 and v==5):
+    print(4)
+elif(u==5 and v==1):
+    print(4)
+elif(u==5 and v==2):
+    print(2)
+else:
+    print("1")

f90a89e5d6aa5305a1ac3d907abb4b52aeb4fab4
SHAMA SHAIK <42835951+Shamabanu@users.noreply.github.com>
2019-02-18 20:10:55 +0530

    Update cookies.py

--- cookies.py
+++ cookies.py
@@ -1,9 +1,26 @@
-a,b=map(int,input().split())
-a1=list(map(int,input().split()))
-a2=list(map(int,input().split()))
-m1=0
-m2=0
-for x in range(0,len(a1)):
-    m1=m1+a1[x]
-    m2=m2+a2[x]
-print(m2//m1)
+import sys
+n,k=map(int,input().split())
+nd=list(map(int,input().split()))
+al=list(map(int,input().split()))
+ck=0
+f=0
+if n==1:
+  print((al[0]+k)//nd[0])
+  sys.exit(0)
+while f!=1:
+  c=0
+  for i in range(n):
+    if al[i]>=nd[i]:
+      al[i]=al[i]-nd[i]
+      c+=1
+    else:
+      if k>=nd[i]-al[i]:
+        k-=nd[i]-al[i]
+        al[i]=0
+        c+=1
+      else:
+        f=1
+        break
+  if c==n:
+    ck+=1
+print(ck)

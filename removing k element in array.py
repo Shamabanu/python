16d5fb6f1aebdc4746d7a0451fe7ff52462d7c66
SHAMA SHAIK <42835951+Shamabanu@users.noreply.github.com>
2019-02-19 21:46:07 +0530

    Create removing k element in array.py

---
+++ removing k element in array.py
@@ -0,0 +1,11 @@
+num,m=map(int,input().split())
+l1=input().split()
+c=[]
+for x in range(0,len(l1)):
+    if(int(l1[x])!=m):
+        c.append(int(l1[x]))
+if(len(c)>0):
+    k=" ".join(map(str,c))
+    print(k)
+else:
+    print(empty)

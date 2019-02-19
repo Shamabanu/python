09d57a0d176112caa864ac75826615c0dc7258b7
SHAMA SHAIK <42835951+Shamabanu@users.noreply.github.com>
2019-02-19 21:25:24 +0530

    Update min distance between elements in an array.py

--- min distance between elements in an array.py
+++ min distance between elements in an array.py
@@ -1,12 +1,16 @@
-num=int(input())
-l1=input().split()
-sum=[]
-x,y=map(int,input().split())
-for j in l1:
-    sum.append(int(j))
-p=sum.index(x)
-q=sum.index(y)
-if(p>q):
-    print(p-q)
-else:
-    print(q-p)
+num= int(input(""))
+arr = list(map(int,input().split()))
+u,v = map(int,input().split())
+x =[index for index, value in enumerate(arr) if value == u]
+y = [index for index, value in enumerate(arr) if value == v]
+dist = num
+for i in range(len(x)):
+    for j in range(len(y)):
+        if(x[i]>y[j]):
+            d = x[i]-y[j]
+        else:
+            d = y[j]-x[i]
+        if(d<dist):
+            dist = d
+print(dist)
+

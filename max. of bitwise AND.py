c9b5e91ef0b5e022daf1ff5b118e5574c99f94f8
SHAMA SHAIK <42835951+Shamabanu@users.noreply.github.com>
2019-02-22 10:26:32 +0530

    Create max. of bitwise AND.py

---
+++ max. of bitwise AND.py
@@ -0,0 +1,8 @@
+num = int(input())
+arr = list(map(int,input().split()))
+max = 0
+for x in range(num):
+    for y in range(num):
+        if(arr[x]&arr[y] > max):
+            max = arr[x]&arr[y]
+print(max)

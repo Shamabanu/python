5a4e27a55697337b104cf7a652dc8188db6f7887
SHAMA SHAIK <42835951+Shamabanu@users.noreply.github.com>
2019-07-26 15:23:03 +0530

    Create number pattern1.c

---
+++ number pattern1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    int N, i, j;
+
+    printf("Enter N: ");
+    scanf("%d", &N);
+    for(i=N; i>=1; i--)
+    {
+        for(j=N; j>i; j--)
+        {
+            printf("%d", j);
+        }
+        for(j=1; j<=(i*2-1); j++)
+        {
+            printf("%d", i);
+        }
+        for(j=i+1; j<=N; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+    for(i=1; i<N; i++)
+    {
+        for(j=N; j>i; j--)
+        {
+            printf("%d", j);
+        }
+        for(j=1; j<=(i*2-1); j++)
+        {
+            printf("%d", i+1);
+        }
+        for(j=i+1; j<=N; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
